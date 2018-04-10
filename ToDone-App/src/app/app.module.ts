47f75c0c2e46d6eb76eded1613e1b430e8e7ab33
JPhrygian96 <jrleach@email.sc.edu>
2018-04-10 13:22:43 -0400

    Merge branch 'master' of https://github.com/ttpalmer/ToDone-App

--- ToDone-App/src/app/app.module.ts
+++ ToDone-App/src/app/app.module.ts
@@ -14,11 +14,8 @@ import { LaunchPage } from "./../pages/launch/launch";
 import { AddGoalPage } from './../pages/addgoal/addgoal';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireAuthModule } from 'angularfire2/auth';
-<<<<<<< HEAD
 import { Data } from '../providers/data/data';
-=======
 import { NgxErrorsModule } from '@ultimate/ngxerrors';
->>>>>>> 4218e4220894b05857e355299e13415ea5761550
 
 
 
